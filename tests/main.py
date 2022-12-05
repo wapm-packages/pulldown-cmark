ad672e56388c7f695ae499ae184cb0f407bd29d2
Rudra <r82941037@gmail.com>
2022-12-05 14:57:04 +0000

    workflows and test

--- tests/main.py
+++ tests/main.py
@@ -1,4 +1,52 @@
+from pulldown_cmark import bindings
+from pulldown_cmark.bindings.pulldown import Options, OffsetItem
+
+pulldown_cmark=bindings.pulldown()
 
 
-def basic_version_test():
-    assert(5,5)
+def test_markdown_to_html():
+    markdown_string="""
+hello
+=====
+* alpha
+* beta
+"""
+    html_string=pulldown_cmark.markdown_to_html(markdown_string)
+
+    assert(html_string,"""<h1>hello</h1>
+<ul>
+<li>alpha</li>
+<li>beta</li>
+</ul>
+""")
+
+def parser_with_option_strikethrough():
+    markdown_string="Hello world, this is a ~~complicated~~ *very simple* example."
+
+    option=Options.ENABLE_STRIKETHROUGH
+
+    iters=pulldown_cmark.parse_with_options(markdown_string,option);
+    print(iters)
+
+def html_test():
+    markdown_string="""Little header
+<script type="text/js">
+function some_func() {
+console.log("teeeest");
+}
+function another_func() {
+console.log("fooooo");
+}
+</script>"""
+
+    expected="""<p>Little header</p>
+<script type="text/js">
+function some_func() {
+console.log("teeeest");
+}
+function another_func() {
+console.log("fooooo");
+}
+</script>"""
+    result = pulldown_cmark.markdown_to_html(markdown_string)
+    assert(expected,result)
